7dda8150b957db2ebd319b04b366a683cf5098f5
Teresa Bartolomei <39525701+Perasperad@users.noreply.github.com>
2021-12-06 18:05:09 +0100

    Update and rename Run_Script_0-4gg.bat to Run_Script_0-4days.bat

---
+++ Run_Script_0-4days.bat
@@ -0,0 +1,4 @@
+cd /D "%~dp0"
+"C:\Users\*your username*\AppData\Local\Programs\Python\Python36\python.exe" "C:\Users\*your username*\Desktop\*your folder*\0-4 days.py"
+pause
+
--- Run_Script_0-4gg.bat
+++
@@ -1,4 +0,0 @@
-cd /D "%~dp0"
-"C:\Users\tbart\AppData\Local\Programs\Python\Python36\python.exe" "C:\Users\tbart\Desktop\prenotazione\test_prenotazione - 6.0 - 0-4 gg.py"
-pause
-
